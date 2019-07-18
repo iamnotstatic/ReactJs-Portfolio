116cb2ac6958059a7625f14aa26641d833e59076
Abdul-fatai <staticdev20046@gmail.com>
2019-07-18 22:09:50 +0100

    Updates

---
+++ precache-manifest.9f9204ced714f083ac301ae4686e523c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f845453eaf343baf481d7aa8693c1d6",
+    "url": "/Abdul-fatai/Portfolio/index.html"
+  },
+  {
+    "revision": "a3b4e7bd71badd306323",
+    "url": "/Abdul-fatai/Portfolio/static/css/2.46fa11a7.chunk.css"
+  },
+  {
+    "revision": "0cc11ed21c25b5f7477d",
+    "url": "/Abdul-fatai/Portfolio/static/css/main.c6af09f3.chunk.css"
+  },
+  {
+    "revision": "a3b4e7bd71badd306323",
+    "url": "/Abdul-fatai/Portfolio/static/js/2.8e39d09d.chunk.js"
+  },
+  {
+    "revision": "0cc11ed21c25b5f7477d",
+    "url": "/Abdul-fatai/Portfolio/static/js/main.036f2572.chunk.js"
+  },
+  {
+    "revision": "c78ffdde6a41491ee0ae",
+    "url": "/Abdul-fatai/Portfolio/static/js/runtime~main.c9f659c9.js"
+  }
+]);
